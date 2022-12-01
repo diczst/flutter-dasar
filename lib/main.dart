56dd6a179b16533d525b4e4edf7b28cef22bb26c
dzuerst <dzuerst@gmail.com>
2022-12-01 18:49:14 +0700

    date format

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,31 @@
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
   State<MyApp> createState() => _MyAppState();
 }
-class _MyAppState extends State<MyApp> {
-  int counter = 1;
 
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text("Stateful Widget"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Date Format"),
+        ),
+        body: Center(
+          child: Text(DateFormat.yMMMMEEEEd().add_Hms().format(DateTime.now())),
+        ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text(
-            counter.toString(),
-            style: TextStyle(fontSize: 30),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              TextButton(
-                  onPressed: () {
-                    setState(() {
-                      counter++;
-                    });
-                  },
-                  child: Icon(Icons.add)),
-              TextButton(
-                  onPressed: () {
-                    setState(() {
-                      counter--;
-                    });
-                  },
-                  child: Icon(Icons.remove))
-            ],
-          )
-        ],
-      ),
-    ));
+    );
   }
 }
+
